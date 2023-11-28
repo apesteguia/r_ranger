51f19154bfb1f21d7bb096de4148f19c666cc567
apesteguia <mikeltxomikeltxo@gmail.com>
2023-11-28 17:57:38 +0100

    scroll primitivo

--- src/ui.rs
+++ src/ui.rs
@@ -14,6 +14,7 @@ pub struct Ui {
     pub length: u32,
     pub idx: i32,
     pub horizontal: i32,
+    pub size: Rect,
     pub terminal: Terminal<CrosstermBackend<io::Stderr>>,
 }
 
@@ -21,6 +22,7 @@ impl Ui {
     pub fn new() -> Result<Self, Box<dyn error::Error>> {
         io::stdout().execute(crossterm::terminal::EnterAlternateScreen)?;
         crossterm::terminal::enable_raw_mode()?;
+        let terminal = Terminal::new(CrosstermBackend::new(io::stderr()))?;
         let binding = std::env::current_dir()?;
         let current = binding.as_path();
         let dir = Dir::from(current)?;
@@ -29,19 +31,22 @@ impl Ui {
             length: 0,
             idx: 0,
             horizontal: 0,
-            terminal: Terminal::new(CrosstermBackend::new(io::stderr()))?,
+            size: terminal.backend().size()?,
+            terminal,
         })
     }
 
     pub fn from(path: &str) -> Result<Self, Box<dyn error::Error>> {
         io::stdout().execute(crossterm::terminal::EnterAlternateScreen)?;
         crossterm::terminal::enable_raw_mode()?;
+        let terminal = Terminal::new(CrosstermBackend::new(io::stderr()))?;
         Ok(Self {
             dir: Dir::from(Path::new(path))?,
             length: 0,
             idx: 0,
             horizontal: 0,
-            terminal: Terminal::new(CrosstermBackend::new(io::stderr()))?,
+            size: terminal.backend().size()?,
+            terminal,
         })
     }
 
@@ -53,40 +58,45 @@ impl Ui {
         self.dir.order_alphabetically();
         let mut list_items = Vec::<ListItem>::new();
 
-        self.dir.files.iter().enumerate().for_each(|(i, f)| {
-            let (item_text, text_color, background_color) = if f.is_dir {
-                (
-                    format!("{} {} {}", f.permissions, f.name, f.last_modified),
-                    Color::Blue,
-                    if self.idx == i.try_into().unwrap() {
-                        Color::White
-                    } else {
-                        Color::Reset
-                    },
-                )
-            } else {
-                (
-                    format!(
-                        "{} {} {}bytes {}",
-                        f.permissions, f.name, f.size, f.last_modified
-                    ),
-                    Color::Green,
-                    if self.idx == i.try_into().unwrap() {
-                        Color::White
-                    } else {
-                        Color::Reset
-                    },
-                )
-            };
+        self.dir
+            .files
+            .iter()
+            .enumerate()
+            .filter(|(i, _f)| i32::try_from(*i).unwrap() >= self.idx)
+            .for_each(|(i, f)| {
+                let (item_text, text_color, background_color) = if f.is_dir {
+                    (
+                        format!("{} {} {}", f.permissions, f.name, f.last_modified),
+                        Color::Blue,
+                        if self.idx == i.try_into().unwrap() {
+                            Color::White
+                        } else {
+                            Color::Reset
+                        },
+                    )
+                } else {
+                    (
+                        format!(
+                            "{} {} {}bytes {}",
+                            f.permissions, f.name, f.size, f.last_modified
+                        ),
+                        Color::Green,
+                        if self.idx == i.try_into().unwrap() {
+                            Color::White
+                        } else {
+                            Color::Reset
+                        },
+                    )
+                };
 
-            list_items.push(ListItem::new(Line::from(Span::styled(
-                item_text,
-                Style::default()
-                    .add_modifier(Modifier::BOLD)
-                    .fg(text_color)
-                    .bg(background_color), // Set background color here
-            ))));
-        });
+                list_items.push(ListItem::new(Line::from(Span::styled(
+                    item_text,
+                    Style::default()
+                        .add_modifier(Modifier::BOLD)
+                        .fg(text_color)
+                        .bg(background_color), // Set background color here
+                ))));
+            });
 
         let list = List::new(list_items);
 
